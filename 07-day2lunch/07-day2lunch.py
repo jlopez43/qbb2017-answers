a59636d612e234ecc13415d01ea8eabcf128a710
Jessica Lopez <jlopez43@jhu.edu>
2017-08-29 13:47:36 -0400

    Add Exercise 1

---
+++ 07-day2lunch/07-day2lunch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+##fh = open( "/Users/cmdb/data/genomes/893_alignment.sam")
+fh = sys.stdin
+# fh = open(sys.argv[1])
+
+string = "NM:i"
+count = 0
+
+
+for line in fh:
+    if string in line:
+        count = count + 1
+
+print count
